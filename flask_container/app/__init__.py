07bc9dfe31f370079b06f4bebd0ae9e051a96f51
brunogeek9 <brunogeek9@gmail.com>
2019-01-21 21:32:21 -0300

    estruturando o projeto

---
+++ flask_container/app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
+app = Flask(__name__)
+
+from controllers import default
